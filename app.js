b2b8b78b2af21aac5300046b9e9d41e2dfbf86fc
Adam Borowski <adabo@itu.dk>
2022-02-14 20:44:50 +0100

    Upgraded to an Express setup

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 const http = require("http");
 const fs = require("fs");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const app = express();
 
-  // the nothing route
-  if (url === "/") {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title><head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title><head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
+app.use((req, res, next) => {
+  // this gets executed for every incoming request
+  console.log("In a middleware");
+  next(); // move to next middleware
 });
+
+app.use((req, res, next) => {
+  // this gets executed for every incoming request
+  console.log("In another middleware");
+  res.write("<h1>hello</h1>");
+  res.end;
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
