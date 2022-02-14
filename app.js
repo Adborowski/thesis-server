55b53fd7f317707dcd6c3aa347bae4e15ccb3660
Adam Borowski <adabo@itu.dk>
2022-02-14 22:20:22 +0100

    Added Haruomi Hosono

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/data", (req, res, next) => {
 app.use("/", (req, res, next) => {
   console.log("In another middleware!");
   res.send(
-    "<h3 style='max-width: 300px; font-family: Helvetica'>Jeśli chcecie śledzić rozwój mojej apki, zapraszam na moją nową specjalną stronę www </h3>"
+    "<h3 style='max-width: 300px; font-family: Helvetica'>Jeśli chcecie śledzić rozwój mojej apki, zapraszam na moją nową specjalną stronę www </h3><img style='max-width: 300px' src='https://thevinylfactory.com/wp-content/uploads/2020/08/Haruomi-Hosono-Takahiko-Ishikawa-and-Masataka-Matsutoy-the-aegean-sea-riessue-vinyl.jpg'>"
   );
 });
 
